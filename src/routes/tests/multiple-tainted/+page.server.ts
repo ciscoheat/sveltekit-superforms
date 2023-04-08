56712310e6b8f7d97ccd65cf351fbe179ad88bcb
Andreas Söderlund <ciscoheat@gmail.com>
2023-04-08 17:04:04 +0200

    Multiple tainted test.

---
+++ src/routes/tests/multiple-tainted/+page.server.ts
@@ -0,0 +1,19 @@
+import type { Actions, PageServerLoad } from './$types';
+import { superValidate } from '$lib/server';
+import type { z } from 'zod';
+import { schema } from './schemas';
+
+export const load = (async (event) => {
+  const form = await superValidate(event, schema);
+  return { form };
+}) satisfies PageServerLoad;
+
+export const actions = {
+  default: async (event) => {
+    const formData = await event.request.formData();
+    const form = await superValidate(formData, schema);
+    console.log('POST', form);
+
+    return { form };
+  }
+} satisfies Actions;

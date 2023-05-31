53cd5b9a7f167d230c97564093a35982a6c5c8a4
Andreas Söderlund <ciscoheat@gmail.com>
2023-05-31 17:22:43 +0200

    Added tests for reset with data

--- src/routes/reset/+page.svelte
+++ src/routes/reset/+page.svelte
@@ -24,16 +24,30 @@
   $: form = superF.form;
   $: enhance = superF.enhance;
   $: errors = superF.errors;
+  $: reset = superF.reset;
+
+  function resetForm() {
+    console.log('Reset!');
+    reset({
+      data: {
+        points: 4
+      }
+    });
+  }
 </script>
 
 <SuperDebug data={$form} />
 
 <div>Resets: {resets}</div>
 
+<div>Points: {$form.points}</div>
+
 <form method="POST" use:enhance>
+  <input type="hidden" name="points" value="0" /><br />
   <input type="text" name="name" bind:value={$form.name} /><br />
   {#if $errors.name}<span>{$errors.name}</span><br />{/if}
   <button>Submit</button>
+  <button type="button" on:click={resetForm}>Client reset</button>
 </form>
 
 <style lang="scss">

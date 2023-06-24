26b3c8b4ca116cb15127e47e5927a36b0012c07a
Andreas Söderlund <ciscoheat@gmail.com>
2023-06-24 11:50:26 +0200

    Svelte 4 compatibility.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,9 +5,10 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
+    'plugin:svelte/recommended',
     'plugin:dci-lint/recommended'
   ],
-  plugins: ['svelte3', '@typescript-eslint'],
+  plugins: ['@typescript-eslint'],
   ignorePatterns: ['*.cjs'],
   overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
   settings: {
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ Headlines: Added, Changed, Deprecated, Removed, Fixed, Security
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.1.2] - 2023-06-24
+
+### Added
+
+- Svelte 4 compatibility.
+
 ## [1.1.1] - 2023-06-20
 
 ### Fixed

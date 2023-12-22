49f8e1659293ea1e44c6002692d9182cffc69d42
Andreas Söderlund <ciscoheat@gmail.com>
2023-12-22 04:36:15 +0100

    Using klona for object cloning.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,18 +1,20 @@
-import { parse, stringify } from 'devalue';
+import { klona } from 'klona';
 
 // Thanks to: https://dev.to/tylim88/typescript-numeric-range-type-15a5#comment-22mld
 export type NumericRange<
-    START extends number,
-    END extends number,
-    ARR extends unknown[] = [],
-    ACC extends number = never
+  START extends number,
+  END extends number,
+  ARR extends unknown[] = [],
+  ACC extends number = never
 > = ARR['length'] extends END
-    ? ACC | START | END
-    : NumericRange<START, END, [...ARR, 1], ARR[START] extends undefined ? ACC : ACC | ARR['length']>
+  ? ACC | START | END
+  : NumericRange<
+      START,
+      END,
+      [...ARR, 1],
+      ARR[START] extends undefined ? ACC : ACC | ARR['length']
+    >;
 
 export function clone<T>(data: T): T {
-  if ('structuredClone' in globalThis) {
-    return structuredClone(data);
-  }
-  return parse(stringify(data));
+  return klona(data);
 }

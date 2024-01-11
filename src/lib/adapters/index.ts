91e16c27e956b555817162142c74146893b012e0
Andreas Söderlund <ciscoheat@gmail.com>
2024-01-11 10:09:48 +0100

    Three adapters are shaked properly.

--- src/lib/adapters/index.ts
+++ src/lib/adapters/index.ts
@@ -1,10 +1,12 @@
 export type { ValidationAdapter } from './adapters.js';
 
-export { ajv } from './ajv.js';
 export { arktype } from './arktype.js';
-export { joi } from './joi.js';
 export { superform } from './superform.js';
-export { typebox } from './typebox.js';
-export { valibot } from './valibot.js';
-export { yup } from './yup.js';
 export { zod } from './zod.js';
+export { valibot } from './valibot.js';
+/*
+export { ajv } from './ajv.js';
+export { joi } from './joi.js';
+export { typebox } from './typebox.js';
+export { yup } from './yup.js';
+*/

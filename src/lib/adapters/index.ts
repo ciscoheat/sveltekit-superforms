3119e04eb7ce51327d8ad62398d813a1354f2877
Andreas Söderlund <ciscoheat@gmail.com>
2024-01-11 10:33:53 +0100

    Thank you TypeSchema! Bundling seems to work with adapters now.

--- src/lib/adapters/index.ts
+++ src/lib/adapters/index.ts
@@ -4,9 +4,9 @@ export { arktype } from './arktype.js';
 export { superform } from './superform.js';
 export { zod } from './zod.js';
 export { valibot } from './valibot.js';
+export { typebox } from './typebox.js';
 /*
 export { ajv } from './ajv.js';
 export { joi } from './joi.js';
-export { typebox } from './typebox.js';
 export { yup } from './yup.js';
 */
--- src/lib/adapters/typebox.ts
+++ src/lib/adapters/typebox.ts
@@ -1,20 +1,29 @@
 import { type ValidationAdapter, createAdapter } from './adapters.js';
 import type { Infer } from '$lib/index.js';
-import { type TSchema, FormatRegistry } from '@sinclair/typebox';
-import { type TypeCheck, TypeCompiler } from '@sinclair/typebox/compiler';
+import type { TSchema } from '@sinclair/typebox';
+import type { TypeCheck } from '@sinclair/typebox/compiler';
 import { memoize } from '$lib/memoize.js';
 
 // From https://github.com/sinclairzx81/typebox/tree/ca4d771b87ee1f8e953036c95a21da7150786d3e/example/formats
 const Email =
 	/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i;
 
-/* @__NO_SIDE_EFFECTS__ */
+const fetchModule = /* @__PURE__ */ memoize(async () => {
+	const { TypeCompiler } = await import(/* webpackIgnore: true */ '@sinclair/typebox/compiler');
+	const { FormatRegistry } = await import(/* webpackIgnore: true */ '@sinclair/typebox');
+	return { TypeCompiler, FormatRegistry };
+});
+
+const { TypeCompiler, FormatRegistry } = await fetchModule();
+
 function _typebox<T extends TSchema>(schema: T): ValidationAdapter<Infer<T>> {
 	if (!compiled.has(schema)) {
 		compiled.set(schema, TypeCompiler.Compile(schema));
 	}
 
-	if (!FormatRegistry.Has('email')) FormatRegistry.Set('email', (value) => Email.test(value));
+	if (!FormatRegistry.Has('email')) {
+		FormatRegistry.Set('email', (value) => Email.test(value));
+	}
 
 	return createAdapter({
 		superFormValidationLibrary: 'typebox',

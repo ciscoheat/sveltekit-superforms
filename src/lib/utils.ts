271fc8f9b55f128112ed60f3ef6b6ef314ba23b1
Andreas Söderlund <ciscoheat@gmail.com>
2024-01-14 14:31:10 +0100

    Using klona/full for cloning. Fixes #312

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-import { klona } from './klona.js';
+import { klona } from 'klona/full';
 
 // Thanks to: https://dev.to/tylim88/typescript-numeric-range-type-15a5#comment-22mld
 export type NumericRange<

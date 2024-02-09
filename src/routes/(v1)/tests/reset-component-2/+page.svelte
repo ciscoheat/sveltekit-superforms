f7ac86d000aab07650c3c0e02e70f29a2548ddba
Andreas Söderlund <ciscoheat@gmail.com>
2024-02-09 09:58:25 +0100

    WIP: Tests work, but reset-component-2 test needs to change depending on Svelte version.

--- src/routes/(v1)/tests/reset-component-2/+page.svelte
+++ src/routes/(v1)/tests/reset-component-2/+page.svelte
@@ -3,12 +3,13 @@
 
 	import SuperForm from './Form.svelte';
 	import TextField from './TextField.svelte';
-	import type { PageData } from './$types.js';
 	import { zod } from '$lib/adapters/zod.js';
+	import type { Snippet } from 'svelte';
 
-	export let data: PageData;
+	let visible = $state(true);
+	let { data } = $props();
 
-	let visible = true;
+	type MsgType = { status: number; text: string };
 </script>
 
 <h3>Multiple componentized Forms</h3>
@@ -16,7 +17,27 @@
 
 <h4>Register Form</h4>
 
-<button on:click={() => (visible = !visible)}>Toggle</button>
+<button onclick={() => (visible = !visible)}>Toggle</button>
+
+{#snippet msg(message)}
+	{#if message}
+		<div
+			class="status"
+			class:error={message.status >= 400}
+			class:success={!message.status || message.status < 300}
+		>
+			{message.text}
+		</div>
+	{/if}
+{/snippet}
+
+{#snippet fields(form)}
+	<TextField type="text" {form} field="name" label="Name" />
+	<TextField type="text" {form} field="email" label="E-Mail" />
+	<p>
+		<button type="submit">submit</button>
+	</p>
+{/snippet}
 
 {#if visible}
 	<!-- SuperForm with dataType 'form' -->
@@ -25,25 +46,9 @@
 		schema={zod(registerSchema)}
 		data={data.regForm}
 		invalidateAll={false}
-		let:form
-		let:message
-	>
-		{#if message}
-			<div
-				class="status"
-				class:error={message.status >= 400}
-				class:success={!message.status || message.status < 300}
-			>
-				{message.text}
-			</div>
-		{/if}
-
-		<TextField type="text" {form} field="name" label="Name" />
-		<TextField type="text" {form} field="email" label="E-Mail" />
-		<p>
-			<button type="submit">submit</button>
-		</p>
-	</SuperForm>
+		{msg}
+		{fields}
+	></SuperForm>
 {/if}
 
 <style>
--- src/routes/(v1)/tests/reset-component-2/Form.svelte
+++ src/routes/(v1)/tests/reset-component-2/Form.svelte
@@ -2,17 +2,39 @@
 	import type { ValidationAdapter, Infer } from '$lib/adapters/adapters.js';
 	import type { registerSchema } from './schema.js';
 	import SuperDebug from '$lib/client/SuperDebug.svelte';
-	import { superForm, type SuperValidated } from '$lib/client/index.js';
+	import { superForm, type SuperForm, type SuperValidated } from '$lib/client/index.js';
+	import type { Snippet } from 'svelte';
 
+	type T = Infer<typeof registerSchema>;
+
+	let {
+		action,
+		schema,
+		data,
+		invalidateAll,
+		msg,
+		fields,
+		dataType = 'form'
+	} = $props<{
+		action: string;
+		schema: ValidationAdapter<T>;
+		data: SuperValidated<T>;
+		invalidateAll: boolean;
+		dataType?: 'form' | 'json';
+		msg: Snippet<[{ status: number; text: string }]>;
+		fields: Snippet<[SuperForm<T>]>;
+	}>();
+
+	/*
 	export let action: string;
 	export let schema: ValidationAdapter<Infer<typeof registerSchema>>;
 	export let data: SuperValidated<Infer<typeof registerSchema>>;
 	export let invalidateAll: boolean; // set to false to keep form data using muliple forms on a page
-
 	export let dataType: 'form' | 'json' = 'form';
+	*/
 
 	export const _form = superForm(data, {
-		dataType: dataType,
+		dataType,
 		validators: schema ? schema : undefined,
 		invalidateAll,
 		onError({ result }) {
@@ -28,17 +50,12 @@
 		}
 	});
 
-	const { form, message, delayed, errors, allErrors, enhance } = _form;
+	const { form, message, enhance } = _form;
 </script>
 
-<form method="POST" use:enhance {...$$restProps}>
-	<slot
-		form={_form}
-		message={$message}
-		errors={$errors}
-		allErrors={$allErrors}
-		delayed={$delayed}
-	/>
+<form method="POST" {action} use:enhance>
+	{@render msg($message)}
+	{@render fields(_form)}
 </form>
 
 <SuperDebug data={$form} />

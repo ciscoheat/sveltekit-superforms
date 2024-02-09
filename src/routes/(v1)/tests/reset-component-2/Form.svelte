2075a2391e8fbe307f2c30242b60784820db0bc4
Andreas Söderlund <ciscoheat@gmail.com>
2024-02-09 05:53:27 +0100

    Fixed component in test

--- src/routes/(v1)/tests/reset-component-2/Form.svelte
+++ src/routes/(v1)/tests/reset-component-2/Form.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
-	import type { ValidationAdapter } from '$lib/adapters/adapters.js';
+	import type { ValidationAdapter, Infer } from '$lib/adapters/adapters.js';
+	import type { registerSchema } from './schema.js';
 	import SuperDebug from '$lib/client/SuperDebug.svelte';
+	import { superForm, type SuperValidated } from '$lib/client/index.js';
 
-	import { superForm } from '$lib/client/index.js';
-	export let data;
-	export let schema: ValidationAdapter<Record<string, unknown>> | undefined = undefined;
+	export let action: string;
+	export let schema: ValidationAdapter<Infer<typeof registerSchema>>;
+	export let data: SuperValidated<Infer<typeof registerSchema>>;
+	export let invalidateAll: boolean; // set to false to keep form data using muliple forms on a page
+
 	export let dataType: 'form' | 'json' = 'form';
-	export let invalidateAll; // set to false to keep form data using muliple forms on a page
 
 	export const _form = superForm(data, {
 		dataType: dataType,
 		validators: schema ? schema : undefined,
-		invalidateAll: invalidateAll,
+		invalidateAll,
 		onError({ result }) {
 			$message = {
 				text: result?.error?.message,

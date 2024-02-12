b3f5a3ee16bf539dd08c6de37bdc0f7b99045441
null <null>
2024-02-12 12:23:56 +0100

    add: base vine implementation

---
+++ src/lib/adapters/vine.ts
@@ -0,0 +1,58 @@
+import {
+	type AdapterOptions,
+	type ValidationAdapter,
+	type Infer,
+	type InferIn,
+	createAdapter,
+	type ValidationResult,
+	type ClientValidationAdapter,
+    type ValidationIssue
+} from './adapters.js';
+import { memoize } from '$lib/memoize.js';
+import { Vine, errors,  } from "@vinejs/vine"
+import type { SchemaTypes } from '@vinejs/vine/types';
+
+async function validate<T extends SchemaTypes>(schema:T, data:unknown): Promise<ValidationResult<Infer<T>>> {
+    let res = null;
+    try {
+        const output = await new Vine().validate({schema, data})
+        res = {
+            success:true,
+            data: output as Infer<T>
+        }
+    } catch (e) {
+        if (e instanceof errors.E_VALIDATION_ERROR) {
+            console.log(e.messages)
+            res = {
+                success:false,
+                issues: e.messages.map((m) => ({
+                    path: m.field as string,
+                    message: m.message as string
+                })) as Array<ValidationIssue>
+            }
+          }
+    } finally {
+        if (res === null) {
+            return {success:false, issues:[]}
+        }
+        return res as ValidationResult<Infer<T>>
+    }
+}
+
+function _vine<T extends SchemaTypes>(schema:T): ValidationAdapter<Infer<T>, InferIn<T>> {
+    return createAdapter({
+        superFormValidationLibrary:"vine",
+        validate: async(data: unknown) => validate(schema, data),
+        jsonSchema: schema
+    })
+}
+
+function _vineClient<T extends SchemaTypes>(schema:T): ClientValidationAdapter<Infer<T>, InferIn<T>> {
+    return {
+        superFormValidationLibrary:"vine",
+        validate: async (data) => validate(schema, data)
+    }
+}
+
+export const typebox = /* @__PURE__ */ memoize(_vine);
+export const typeboxClient = /* @__PURE__ */ memoize(_vineClient);

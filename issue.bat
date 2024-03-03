b47ebccd53425cabb9cd35fb537a638f6ddbe48a
Andreas Söderlund <ciscoheat@gmail.com>
2024-03-03 16:29:37 +0100

    Added batch file for fixing imports in stackblitz issues.

---
+++ issue.bat
@@ -0,0 +1,4 @@
+cd src\routes\(v2)\v2\%1
+sed -i "s#'sveltekit-superforms'#'$lib/index.js'#g" *.*
+sed -i -E "s#import \{ ([a-z]+) \} from 'sveltekit-superforms/adapters'#import { \1 } from '$lib/adapters/\1.js'#g" *.*
+sed -i -E "s#import \{ ([a-z]+)Client \} from 'sveltekit-superforms/adapters'#import { \1Client } from '$lib/adapters/\1.js'#g" *.*

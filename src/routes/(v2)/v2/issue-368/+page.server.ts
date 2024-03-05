32a75f834400982785819634580d7f74fd99c581
Andreas Söderlund <ciscoheat@gmail.com>
2024-03-05 20:28:00 +0100

    check fix

--- src/routes/(v2)/v2/issue-368/+page.server.ts
+++ src/routes/(v2)/v2/issue-368/+page.server.ts
@@ -10,7 +10,7 @@ export const load = async () => {
 
 export const actions = {
 	default: async ({ request }) => {
-		const form = await superValidate(request);
+		const form = await superValidate(request, zod(schema));
 		console.log(form);
 
 		if (!form.valid) return fail(400, { form });

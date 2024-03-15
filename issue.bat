6779fe75ffeb49fb9e29b94dc2677abb5ca08087
Andreas Söderlund <ciscoheat@gmail.com>
2024-03-15 14:33:49 +0100

    Recursive issue batch file

--- issue.bat
+++ issue.bat
@@ -1,5 +1,5 @@
 pushd src\routes\(v2)\v2\%1
-sed -i "s#'sveltekit-superforms'#'$lib/index.js'#g" *.*
-sed -i -E "s#import \{ ([a-z]+) \} from 'sveltekit-superforms/adapters'#import { \1 } from '$lib/adapters/\1.js'#g" *.*
-sed -i -E "s#import \{ ([a-z]+)Client \} from 'sveltekit-superforms/adapters'#import { \1Client } from '$lib/adapters/\1.js'#g" *.*
+grep -rl . | xargs sed -i "s#'sveltekit-superforms'#'$lib/index.js'#g"
+grep -rl . | xargs sed -i -E "s#import \{ ([a-z]+) \} from 'sveltekit-superforms/adapters'#import { \1 } from '$lib/adapters/\1.js'#g"
+grep -rl . | xargs sed -i -E "s#import \{ ([a-z]+)Client \} from 'sveltekit-superforms/adapters'#import { \1Client } from '$lib/adapters/\1.js'#g"
 popd

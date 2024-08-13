891a74a72331333b8f681b071badc75290efce0b
Andreas Söderlund <ciscoheat@gmail.com>
2024-08-13 12:53:45 +0200

    Added test for complicated type error.

---
+++ src/routes/(v2)/v2/component-sf-type/Form.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { type SuperForm } from '$lib/index.js';
+	// Remove "c: string" to show a complicated type error
+	export let superform: SuperForm<{ a: string; b: string; c: string }>;
+</script>
+
+<form method="POST" use:superform.enhance {...$$restProps}></form>

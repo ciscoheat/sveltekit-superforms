0fd089dd819e433fa1627df6ed77135dc0514624
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-09-12 16:28:27 +0200

    add some exports

--- src/lib/adapters/zod.ts
+++ src/lib/adapters/zod.ts
@@ -36,9 +36,9 @@ type ZodObjectUnion<T extends AnyZodObject> = ZodUnion<
 	[ZodValidation<T>, ZodValidation<T>, ...ZodValidation<T>[]]
 >;
 
-type ZodObjectType = ZodType<Record<string, unknown>, ZodTypeDef, Record<string, unknown>>;
+export type ZodObjectType = ZodType<Record<string, unknown>, ZodTypeDef, Record<string, unknown>>;
 
-type ZodObjectTypes = AnyZodObject | ZodObjectUnion<AnyZodObject> | ZodObjectType;
+export type ZodObjectTypes = AnyZodObject | ZodObjectUnion<AnyZodObject> | ZodObjectType;
 
 export type ZodValidation<T extends ZodObjectTypes> =
 	| T

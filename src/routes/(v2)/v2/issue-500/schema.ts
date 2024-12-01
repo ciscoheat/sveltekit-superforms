5388c3db79e914f9ec7078866dca2341f6112c18
Andreas Söderlund <ciscoheat@gmail.com>
2024-12-01 06:36:41 +0100

    Linter

--- src/routes/(v2)/v2/issue-500/schema.ts
+++ src/routes/(v2)/v2/issue-500/schema.ts
@@ -1,23 +1,23 @@
 import { z } from 'zod';
 
 export const inviteUserToGroupSchema = z.object({
-	username: z.string().min(2),
+	username: z.string().min(2)
 });
 
 export const modifyGroupAccessSchema = z.object({
 	users: z
 		.object({
 			username: z.string().min(2),
-			remove: z.boolean(),
+			remove: z.boolean()
 		})
-		.array(),
+		.array()
 });
 
 export const fixedModifyGroupAccessSchema = z.object({
 	users: z
 		.object({
 			username: z.string().min(2),
-			removed: z.boolean(),
+			removed: z.boolean()
 		})
-		.array(),
+		.array()
 });

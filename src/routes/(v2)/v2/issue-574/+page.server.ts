660e7c198c53573bf65b08f349ce6cae4d82bc9c
Andreas Söderlund <ciscoheat@gmail.com>
2025-03-09 14:48:29 +0100

    Added test case for schemasafe bug

---
+++ src/routes/(v2)/v2/issue-574/+page.server.ts
@@ -0,0 +1,32 @@
+import type { Actions, PageServerLoad } from './$types.js';
+
+import { superValidate, message } from '$lib/index.js';
+import { schemasafe } from '$lib/adapters/schemasafe.js';
+import { fail } from '@sveltejs/kit';
+import { schema } from './schema.js';
+
+export const load: PageServerLoad = async () => {
+	const adapter = schemasafe(schema);
+  const defaultValue = {
+    arr: [{ 
+      arr: [{ 
+        tryInvalidValue: ''
+      }] 
+    }]
+  }
+	return { 
+    form: await superValidate(defaultValue, adapter)
+  };
+};
+
+export const actions: Actions = {
+	default: async ({ request }) => {
+		const adapter = schemasafe(schema);
+		const form = await superValidate(request, adapter);
+		console.log(form, form.data.arr[0].arr[0].tryInvalidValue);
+
+		if (!form.valid) return fail(400, { form });
+
+		return message(form, 'Form posted successfully!');
+	}
+};
---
+++ src/routes/(v2)/v2/issue-574/+page.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import { superForm } from '$lib/index.js';
+	import { schemasafe } from '$lib/adapters/schemasafe.js';
+	import SuperDebug from '$lib/index.js';
+	import { schema } from './schema.js';
+
+	let { data } = $props();
+
+	const validators = schemasafe(schema);
+
+	const { form, errors, constraints, message, enhance } = superForm(data.form, {
+		dataType: 'json',
+		resetForm: false,
+		validators,
+		validationMethod: 'oninput'
+	});
+</script>
+
+<SuperDebug data={{ $form, $errors, $constraints }} />
+
+<h3>Superforms testing ground - JSON Schema</h3>
+
+{#if $message}
+	<!-- eslint-disable-next-line svelte/valid-compile -->
+	<div class="status" class:error={page.status >= 400} class:success={page.status == 200}>
+		{$message}
+	</div>
+{/if}
+
+<form method="POST" use:enhance>
+	{#each $form.arr as firstArr, i}
+		{#each firstArr.arr as secondArr, j}
+			<input
+				type="text"
+				bind:value={secondArr.tryInvalidValue}
+				placeholder="Try entering text shorter than 5 chars"
+			/>
+			{#if $errors?.arr?.[i]?.arr?.[j]?.tryInvalidValue}<span class="invalid"
+					>Please enter a valid email</span
+				>{/if}
+		{/each}
+	{/each}
+	<button>Submit</button>
+</form>
+
+<hr />
+<p>
+	💥 <a target="_blank" href="https://superforms.rocks">Created with Superforms for SvelteKit</a> 💥
+</p>
+
+<style>
+	.invalid {
+		color: red;
+	}
+
+	.status {
+		color: white;
+		padding: 4px;
+		padding-left: 8px;
+		border-radius: 2px;
+		font-weight: 500;
+	}
+
+	.status.success {
+		background-color: seagreen;
+	}
+
+	.status.error {
+		background-color: #ff2a02;
+	}
+
+	input {
+		background-color: #ddd;
+	}
+
+	a {
+		text-decoration: underline;
+	}
+
+	hr {
+		margin-top: 4rem;
+	}
+
+	form {
+		padding-top: 1rem;
+		padding-bottom: 1rem;
+	}
+</style>

81d505dc51b7cd8f4e3a24f24782f27f20c93da6
DeKok, Matt G. <matt.dekok@gmail.com>
2025-03-10 08:00:45 -0500

    Add default options to standard schema example

--- src/routes/(v2)/v2/standard-schema/+page.server.ts
+++ src/routes/(v2)/v2/standard-schema/+page.server.ts
@@ -3,15 +3,22 @@ import { message, superValidate } from '$lib/index.js';
 import { fail } from '@sveltejs/kit';
 import { valibotSchema, zodSchema } from './schema.js';
 
+const options = {
+	defaults: {
+		name: 'John Doe',
+		email: 'john.doe@example.com'
+	}
+};
+
 export const load = async () => {
-	const valibotForm = await superValidate(standard(valibotSchema));
-	const zodForm = await superValidate(standard(zodSchema));
+	const valibotForm = await superValidate(standard(valibotSchema, options));
+	const zodForm = await superValidate(standard(zodSchema, options));
 	return { valibotForm, zodForm };
 };
 
 export const actions = {
 	valibot: async ({ request }) => {
-		const form = await superValidate(request, standard(valibotSchema));
+		const form = await superValidate(request, standard(valibotSchema, options));
 		console.log(form);
 
 		if (!form.valid) return fail(400, { form });
@@ -19,7 +26,7 @@ export const actions = {
 		return message(form, 'Form posted successfully!');
 	},
 	zod: async ({ request }) => {
-		const form = await superValidate(request, standard(zodSchema));
+		const form = await superValidate(request, standard(zodSchema, options));
 		console.log(form);
 
 		if (!form.valid) return fail(400, { form });

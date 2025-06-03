32e65903cdc27ef091aa6cf0cc88bdacd1894ce6
Andreas Söderlund <ciscoheat@gmail.com>
2025-06-03 21:56:03 +0200

    Linter

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,84 +1,87 @@
-const {
-    defineConfig,
-    globalIgnores,
-} = require("eslint/config");
+const { defineConfig, globalIgnores } = require('eslint/config');
 
-const tsParser = require("@typescript-eslint/parser");
-const typescriptEslint = require("@typescript-eslint/eslint-plugin");
-const globals = require("globals");
-const parser = require("svelte-eslint-parser");
-const js = require("@eslint/js");
+const tsParser = require('@typescript-eslint/parser');
+const typescriptEslint = require('@typescript-eslint/eslint-plugin');
+const globals = require('globals');
+const parser = require('svelte-eslint-parser');
+const js = require('@eslint/js');
 
-const {
-    FlatCompat,
-} = require("@eslint/eslintrc");
+const { FlatCompat } = require('@eslint/eslintrc');
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all
 });
 
-module.exports = defineConfig([{
-    extends: compat.extends(
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:svelte/recommended",
-        "plugin:dci-lint/recommended",
-        "prettier",
-    ),
+module.exports = defineConfig([
+	{
+		extends: compat.extends(
+			'eslint:recommended',
+			'plugin:@typescript-eslint/recommended',
+			'plugin:svelte/recommended',
+			'plugin:dci-lint/recommended',
+			'prettier'
+		),
 
-    languageOptions: {
-        parser: tsParser,
-        sourceType: "module",
-        ecmaVersion: 2020,
+		languageOptions: {
+			parser: tsParser,
+			sourceType: 'module',
+			ecmaVersion: 2020,
 
-        parserOptions: {
-            extraFileExtensions: [".svelte"],
-        },
+			parserOptions: {
+				extraFileExtensions: ['.svelte']
+			},
 
-        globals: {
-            ...globals.browser,
-            ...globals.node,
-        },
-    },
+			globals: {
+				...globals.browser,
+				...globals.node
+			}
+		},
 
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-    },
+		plugins: {
+			'@typescript-eslint': typescriptEslint
+		},
 
-    rules: {
-        "dci-lint/literal-role-contracts": "off",
-    },
-}, {
-    files: ["**/*.svelte"],
+		rules: {
+			'dci-lint/literal-role-contracts': 'off'
+		}
+	},
+	{
+		files: ['**/*.svelte'],
 
-    languageOptions: {
-        parser: parser,
+		languageOptions: {
+			parser: parser,
 
-        parserOptions: {
-            parser: "@typescript-eslint/parser",
-        },
-    },
-}, {
-    files: ["src/lib/**"],
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	},
+	{
+		files: ['src/lib/**'],
 
-    rules: {
-        "no-console": ["error", {
-            allow: ["warn"],
-        }],
-    },
-}, globalIgnores([
-    "**/.DS_Store",
-    "**/node_modules",
-    "build",
-    ".svelte-kit",
-    "package",
-    "**/.env",
-    "**/.env.*",
-    "!**/.env.example",
-    "dist",
-    "**/pnpm-lock.yaml",
-    "**/package-lock.json",
-    "**/yarn.lock",
-])]);
+		rules: {
+			'no-console': [
+				'error',
+				{
+					allow: ['warn']
+				}
+			]
+		}
+	},
+	globalIgnores([
+		'**/.DS_Store',
+		'**/node_modules',
+		'build',
+		'.svelte-kit',
+		'package',
+		'**/.env',
+		'**/.env.*',
+		'!**/.env.example',
+		'dist',
+		'**/pnpm-lock.yaml',
+		'**/package-lock.json',
+		'**/yarn.lock'
+	])
+]);

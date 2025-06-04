2943030e43c9f33c1ba13e1ffc259cda3e8b2161
Andreas Söderlund <ciscoheat@gmail.com>
2025-06-04 16:20:09 +0200

    Zod mini schema fix

--- src/routes/(v2)/v2/zodmini/schema.ts
+++ src/routes/(v2)/v2/zodmini/schema.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod/v4-mini';
 
 export const schema = z.object({
-	name: z.string().check(z.minLength(2, 'Name is too short')),
+	name: z._default(z.string(), 'Hello world!'),
 	email: z.email('Invalid email')
 });

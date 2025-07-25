a32810d069fb71067d6fb0da6e133e654ea8948f
Konstantin Bläsi <kblaesi@gmail.com>
2025-07-25 11:30:33 +0200

    test: issue reproduction forms

---
+++ src/routes/(v2)/v2/issue-622/bad-with-workaround/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import SuperDebug, { superForm } from '$lib/index.js';
+
+	let { data } = $props();
+
+	const form = superForm(data.form, {
+		dataType: 'json',
+		scrollToError: true,
+		autoFocusOnError: 'detect',
+		taintedMessage: false,
+		validationMethod: 'submit-only',
+		resetForm: false,
+		invalidateAll: false,
+		applyAction: true,
+		delayMs: 500,
+		timeoutMs: 10000
+	});
+	const { enhance, submitting, delayed, timeout, reset } = form;
+
+	const time = () => new Date().toISOString();
+
+	page.subscribe((v) => console.log(`${time()}: Page store update`, { page: v }));
+</script>
+
+<form method="post" use:enhance action="/v2/issue-622/bad-with-workaround?start">
+	<button>Submit</button>
+</form>
+
+<SuperDebug data={{ $submitting, $delayed, $timeout }} />
